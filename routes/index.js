83739ff2a93bf38900379aa3231d30f820fc9075
man7788 <jerman1841@gmail.com>
2023-09-30 23:38:40 +0800

    Update index routes

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,31 @@ const express = require("express");
 const router = express.Router();
 
 const sign_up_controller = require("../controllers/signUpController");
+const verification_controller = require("../controllers/verificationController");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Exclusive Clubhouse" });
+  res.render("index", {
+    title: "Exclusive Clubhouse",
+    user: res.locals.currentUser,
+    fail: req.flash("error"),
+  });
 });
 
-router.get("/sign_up", function (req, res, next) {
-  res.render("sign_up", { title: "Sign Up" });
+router.get("/sign-up", function (req, res, next) {
+  res.render("sign-up", { title: "Sign Up" });
 });
 
-router.post("/sign_up", sign_up_controller.submit_form);
+router.post("/sign-up", sign_up_controller.submit_form);
+
+router.get("/verification", function (req, res, next) {
+  if (res.locals.currentUser) {
+    res.render("verification", { title: "Membership Verification" });
+  } else {
+    res.redirect("/");
+  }
+});
+
+router.post("/verification", verification_controller.submit_verification);
 
 module.exports = router;
